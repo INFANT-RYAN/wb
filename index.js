f16d925a7d177d1cffcf9aa9eece36c708fbfb4f
INFANT-RYAN <ini0k17@gmail.com>
2024-08-03 07:18:55 +0100

    feat:scripting & valdation

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-console.log('hello world');
-let firstName = "Infant";
-let lastName = "Ryan"
-console.log(firstName)
-console.log(lastName)
+//selecting
+var Name=document.getElementById("name")
+var PhNo=document.getElementById("phno")
+var mail=document.getElementById("mail")
+var  msg=document.getElementById("msg")
+const pattern = /^\d{10}$/
+var isValid = pattern.test(PhNo)
+function validate()
+{
+    if(Name.value==""){
+        alert("Fill the Name")
+    }
+    if(msg.value==""){
+        alert("msg field should be filled")
+    }
+}
